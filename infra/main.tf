a304280ce1602396cd3478d445fde874d0a37f20
Ismail Yushaw <isyuus@gmail.com>
2023-07-14 00:58:03 +0000

    Added a 2 working database vm and their firewall rules.

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,7 @@
 
+# App Server
 resource "google_compute_instance" "app1" {
-  name = "application-server=${count.index}"
+  name = "application1-server"
   machine_type = "f1-micro"
   zone = "us-central1-a"
 
@@ -14,12 +15,7 @@ resource "google_compute_instance" "app1" {
     network = "default"
 
     access_config {
-      egress {
-        egress_rule{
-          to_port = 0
-          to_address = ["0.0.0.0/0"]
-        }
-      }
+      
     }
   }
 
@@ -28,7 +24,8 @@ resource "google_compute_instance" "app1" {
   tags = ["http-server"]
 }
 
-resource "google_compute_firewall" "http-server" {
+# Firewall settings for app 1
+resource "google_compute_firewall" "app1-server" {
   name = "default-allow-http"
   network = "default"
 
@@ -41,6 +38,113 @@ resource "google_compute_firewall" "http-server" {
   target_tags = ["http-server"]
 }
 
-resource "google_compute_instance" "db-server" {
-  
+# App 2
+
+resource "google_compute_instance" "app2" {
+  name = "application2-server"
+  machine_type = "f1-micro"
+  zone = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      
+    }
+  }
+
+  metadata_startup_script = "sudo apt update; sudo apt upgrade -y; sudo apt install nginx -y"
+
+  tags = ["http-server"]
+}
+
+# Firewall settings for app2
+resource "google_compute_firewall" "app2-server" {
+  name = "default-allow-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports = ["80","22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["http-server"]
+}
+
+resource "google_compute_instance" "db_master" {
+  name = "db-master"
+  machine_type = "f1-micro"
+  zone = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      
+    }
+  }
+}
+
+resource "google_compute_firewall" "database-server-access" {
+  name = "default-allow-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports = ["3306","22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["database-server"]
+}
+
+resource "google_compute_instance" "db_replica" {
+  name = "db-replica"
+  machine_type = "f1-micro"
+  zone = "us-cenral1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      
+    }
+  }
+
+  metadata = {
+    "google-compute-engine-disk-encryption-key" = var.gcp_db_server_encryption_key
+  }
+
+  depends_on = [google_compute_instance.db_master]
+}
+
+resource "google_compute_firewall" "database-replica-server" {
+  name = "default-allow-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports = ["3306", "22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["database-server"]
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -9,3 +9,7 @@ variable "gcp_project" {
 variable "gcp_region" {
   
 }
+
+variable "gcp_db_server_encryption_key" {
+  
+}
